d4e0da931714148c1657a54fdae25816b2a3649c
Game <jeerasakananta@gmail.com>
2024-09-18 20:35:17 +0700

    add file

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root():    
+    return {"message": "Hello World 3:"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
