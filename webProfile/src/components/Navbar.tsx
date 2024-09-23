a528b8c4a199b6b555b90c08c5c520ba3d283ef5
Game <jeerasakananta@gmail.com>
2024-09-23 21:29:50 +0700

    add page

---
+++ webProfile/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React    from "react";
+
+const Navbar: React.FC = () => {
+    return (
+        <div className="navbar">
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+export default Navbar;
