b366fc52f05212531da136d9a5752db9aac1ca74
Game <jeerasakananta@gmail.com>
2024-10-22 22:20:32 +0700

    leraing : react

--- react/leraning/src/App.tsx
+++ react/leraning/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,9 +5,9 @@ function App() {
   return (
     <>
       <div>
-        <h1> Hello world ;) </h1>
+        <h1 > Hello world ;) </h1>
       </div>
-  );
     </>
+  );
 }
 export default App;
