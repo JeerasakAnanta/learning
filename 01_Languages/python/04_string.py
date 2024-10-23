563a79c6f948b18c223c24c6f338f155267ae276
Game <jeerasakananta@gmail.com>
2024-10-23 23:21:50 +0700

    learning : python

---
+++ 01_Languages/python/04_string.py
@@ -0,0 +1,26 @@
+s = "mystrin"
+print(s)
+
+print("-" * len(s))
+
+
+a = "hello world ;)"
+print(a)
+
+muitstring = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print("muitstring = ", muitstring)
+
+
+x = "lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt\nut labore et dolore magna aliqua."
+
+# strings are arrays
+sa = "hello world : "
+print("s[0] = ", s[0])
+
+
+# check string
+txt = "The best things in life are free!"
+print("free" in txt)
