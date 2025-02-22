cec1ac0af601686cfdd9191bb7b3b90838267fc5
Game <jeerasakananta@gmail.com>
2025-02-23 00:51:01 +0700

    ci : update config db for project node login

--- node_login/configs/db.js
+++ node_login/configs/db.js
@@ -1,17 +1,18 @@
-// โค้ดสำหรับเชื่อมต่อฐานข้อมูล MySQL ด้วย Node.js  
-x =  require('dotenv').config(); 
-const mysql = require('mysql');
+// โค้ดสำหรับเชื่อมต่อฐานข้อมูล MySQL ด้วย Node.js
+x = require("dotenv").config();
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: 'db', 
-    user: process.env.MYSQL_USERNAME, 
-    password: process.env.MYSQL_USERNAME, 
-    database: process.env.MYSQL_DATABASE
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL connected...');
+db.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL connected...");
 });
 
 module.exports = db;
