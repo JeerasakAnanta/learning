e6951fdeb8fd52e76fdde3153d0af08bb398aa0a
Game <jeerasakananta@gmail.com>
2025-02-24 01:27:34 +0700

    add : update chat models

---
+++ langChain_js/chat_models.js
@@ -0,0 +1,8 @@
+// import ChatOpenAI from "chat-openai";
+
+// const  ChatOpenAI = require("chat-openai");
+// Load environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("OK");
--- langChain_js/openai.js
+++ langChain_js/openai.js
@@ -1,5 +1,17 @@
-import {OpenAI} from '@langchain/core'
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { OpenAIModerationChain } from "langchain/chains";
+// import {OpenAI} from '@langchain/core'
+// import { OpenAIEmbeddings } from "@langchain/openai";
+// import { OpenAIModerationChain } from "langchain/chains";
 
-console.log('hello langchain javascript first time') ;
+const { ChatOpenAI } = require("@langchain/openai");
+const { OpenAIEmbeddings } = require("@langchain/openai");
+
+const model = new ChatOpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  modelName: "gpt-3.5-turbo",
+});
+const embeddings = new OpenAIEmbeddings({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: "text-embedding-ada-002",
+});
+
