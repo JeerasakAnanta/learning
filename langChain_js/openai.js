cb0ab3cf83b5061bbeb610da6c316fa6bd77ee93
Game <jeerasakananta@gmail.com>
2025-02-24 00:17:52 +0700

    new : leran first time with langchain  javaScrtip

---
+++ langChain_js/openai.js
@@ -0,0 +1,5 @@
+import {OpenAI} from '@langchain/core'
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { OpenAIModerationChain } from "langchain/chains";
+
+console.log('hello langchain javascript first time') ;
