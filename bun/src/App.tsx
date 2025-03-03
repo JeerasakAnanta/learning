fcabb497582a24b6e49c598da6cf36a2f1ee40a3
Game <jeerasakananta@gmail.com>
2025-03-03 23:06:02 +0700

    add : initialize bun-react-tailwind template with basic structure and API testing component

---
+++ bun/src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+export function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold text-center p-4">
+        first time with bun
+      </h1>
+    </div>
+  );
+}
+
+export default App;
