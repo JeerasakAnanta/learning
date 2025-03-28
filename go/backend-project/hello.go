03f19caa8d7fc76bcf35a75a7cf10ffd6c1f0e5e
Game <jeerasakananta@gmail.com>
2025-03-28 23:23:06 +0700

    feat: add initial Go program with Hello World output

---
+++ go/backend-project/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type newClass struct {
+	// fields and methods for the new class
+}
+
+func main() {
+	// Print "Hello, World!" to the console
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World ;)")
+}
