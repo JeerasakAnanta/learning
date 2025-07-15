30e1c489665a1e12968e917b42940354d8075aa9
Game <jeerasakananta@gmail.com>
2025-07-15 11:12:48 +0700

    add new api end point

--- api_go/main.go
+++ api_go/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
 func main() {
-	fmt.Println("Hello world ;) ")
-
+	log.Print("Server started on http://localhost:8080")
+	log.Fatal(http.lisstenAndServe(":8080", nil))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
