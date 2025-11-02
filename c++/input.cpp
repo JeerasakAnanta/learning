9790037ce98cceb06f66098896703ab3fdddb61f
Game <jeerasakananta@gmail.com>
2025-11-02 21:48:05 +0700

    c++ :leraning

---
+++ c++/input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // main
+
+    cout << "hello time time \n";
+    cout << "sout 2\n";
+
+    // pritin number
+    cout << "print number\n";
+    cout << 10 << "\n";
+    cout << 2 * 10 << "\n";
+
+    // cout
+
+    cout << "Hello world " << '\n';
+    cout << "I am leaing c++ \n";
+    return 0;
+}
