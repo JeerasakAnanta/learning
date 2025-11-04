4429f13b4617fe142ab7513763a9b321588b5e53
Game <jeerasakananta@gmail.com>
2025-11-04 23:33:03 +0700

    python : data type

---
+++ python/data_type.py
@@ -0,0 +1,9 @@
+# Built-inata types
+# in programming datatype is an important concept
+# varibles can stor data of different types, different types can do different things
+# pythonhas the following  data type s built-in b deault, in these categories
+# text Type  str
+# Numeric Types : int, float, complex
+# Sequence Types : list, typle, range
+# Mapping type: dict
+#
